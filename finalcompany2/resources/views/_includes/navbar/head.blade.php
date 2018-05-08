a82227f8fa1ccf59931a0f33bf379b37ffed1ebe
greatshah222 <great.shah222@gmail.com>
2018-05-08 16:17:50 +0300

    navbar added

---
+++ finalcompany2/resources/views/_includes/navbar/head.blade.php
@@ -0,0 +1,111 @@
+
+<!DOCTYPE html>
+<html lang="zxx">
+
+<head>
+    <title>Bettering an Education Category Bootstrap Responsive Website Template | Home :: w3layouts</title>
+    <!-- Meta Tags -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="keywords" content="Bettering Responsive web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template,
+Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony Ericsson, Motorola web design" />
+    <script type="application/x-javascript">
+        addEventListener("load", function () {
+            setTimeout(hideURLbar, 0);
+        }, false);
+
+        function hideURLbar() {
+            window.scrollTo(0, 1);
+        }
+    </script>
+    <!-- // Meta Tags -->
+    <link href="{{asset('css/bootstrap.css')}}" rel="stylesheet" type="text/css" media="all" />
+    <link href="{{asset('css/font-awesome.min.css')}}" rel="stylesheet" type="text/css" media="all">
+    <link rel="stylesheet" href="{{asset('css/flexslider.css')}}" type="text/css" media="screen" property="" />
+    <!--testimonial flexslider-->
+    <link href="{{asset('css/style.css')}}" rel="stylesheet" type="text/css" media="all" />
+    <!--fonts-->
+    <link href="//fonts.googleapis.com/css?family=Lato:300,400,700,900" rel="stylesheet">
+    <link href="//fonts.googleapis.com/css?family=Raleway:300,400,500,600,800" rel="stylesheet">
+    <!--//fonts-->
+
+</head>
+
+<body>
+<!--Header-->
+<div class="header">
+    <div class="top">
+        <div class="container">
+
+            <div class="col-md-9 top-left">
+                <ul>
+                    <li><i class="fa fa-map-marker" aria-hidden="true"></i> 1143 New York, USA</li>
+                    <li><i class="fa fa-phone" aria-hidden="true"></i> +(010) 221 918 811</li>
+                    <li><i class="fa fa-envelope-o" aria-hidden="true"></i> <a href="mailto:info@example.com">info@example.com</a></li>
+                </ul>
+            </div>
+            <div class="col-md-3 top-middle">
+                <ul>
+                    <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                    <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                    <li><a href="#"><i class="fa fa-google-plus"></i></a></li>
+                    <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+                </ul>
+            </div>
+
+            <div class="clearfix"></div>
+
+        </div>
+    </div>
+    <!--top-bar-w3layouts-->
+    <div class="top-bar-w3layouts">
+        <div class="container">
+            <nav class="navbar navbar-default">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <h1><a href="#">Bettering</a></h1>
+                </div>
+                <!-- navbar-header -->
+                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+
+                    <ul class="nav navbar-nav navbar-right">
+                        <li><a href="#" class="active">Home</a></li>
+                        <li><a href="#">About</a></li>
+                        <li><a href="#">Gallery</a></li>
+                        <li><a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Pages <span class="caret"></span></a>
+                            <ul class="dropdown-menu">
+                                <li><a href="#">Web Icons</a></li>
+                                <li><a href="#">Short Codes</a></li>
+                            </ul>
+                        </li>
+                        <li><a href="#">Contact</a></li>
+                    </ul>
+
+                </div>
+
+
+
+
+                <div class="clearfix"> </div>
+            </nav>
+        </div>
+
+    </div>
+    <!--//top-bar-w3layouts-->
+    <!--Slider-->
+
+
+
+    <script src="{{asset('js/bootstrap.js')}}"></script>
+</div>
+
+
+    <!--// Required Scripts -->
+</body>
+
+</html>
